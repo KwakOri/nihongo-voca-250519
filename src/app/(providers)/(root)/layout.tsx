2db90fa05988142aabf3da243fb15c2d6a7f05cc
kwakjaehun <jh5414092@icloud.com>
2025-05-19 16:43:37 +0900

    Style: 배경색 수정

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="w-full h-full bg-black">
-      <div className="fixed inset-0 p-4">{children}</div>
+      <div className="fixed inset-0 p-4 bg-black">{children}</div>
     </div>
   );
 };
