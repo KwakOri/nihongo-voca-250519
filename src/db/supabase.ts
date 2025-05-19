17f5e5f2ee263e2d91eaec3b9de7218d5cde37e8
kwakjaehun <jh5414092@icloud.com>
2025-05-19 12:43:15 +0900

    Feat: react-query 세팅

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,9 +1,11 @@
 // src/supabaseClient.js
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE_URL => ", SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY => ", SUPABASE_ANON_KEY);
 export const supabase: SupabaseClient = createClient(
   SUPABASE_URL as string,
   SUPABASE_ANON_KEY as string
