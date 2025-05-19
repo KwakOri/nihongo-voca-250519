da98b36102de73457f00120d2188eec626b4d051
kwakjaehun <jh5414092@icloud.com>
2025-05-19 16:39:41 +0900

    Style: Change BG Color to black

--- src/app/(providers)/(root)/layout.tsx
+++ src/app/(providers)/(root)/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full bg-black">
       <div className="fixed inset-0 p-4">{children}</div>
     </div>
   );
