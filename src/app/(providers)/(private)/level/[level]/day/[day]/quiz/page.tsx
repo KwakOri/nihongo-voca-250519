23f24801f33e4f14c99232e8416feec3f3e95fb5
kwakjaehun <jh5414092@icloud.com>
2025-05-26 04:34:41 +0900

    Feat: 퀴즈 유형 선택화면 UI 개선

--- src/app/(providers)/(private)/level/[level]/day/[day]/quiz/page.tsx
+++ src/app/(providers)/(private)/level/[level]/day/[day]/quiz/page.tsx
@@ -5,6 +5,12 @@ import { useRouter } from "next/navigation";
 
 const QuizTypePage = () => {
   const types = [1, 2, 3, 4];
+  const titles = {
+    1: "발음",
+    2: "한자",
+    3: "뜻",
+    4: "문장",
+  };
   const navigate = useRouter();
   const onGoBackButtonClick = () => {
     navigate.back();
@@ -14,7 +20,7 @@ const QuizTypePage = () => {
     localStorage.removeItem("quiz-meta");
   };
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <div className="sticky z-10 top-0 w-full h-12 grid grid-cols-3 gap-2 bg-black pb-2">
         <button
           className={
@@ -31,10 +37,17 @@ const QuizTypePage = () => {
           -
         </div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col h-full gap-2">
         {types.map((type) => (
-          <Link key={type} onClick={onQuizTypeClick} href={`quiz/${type}`}>
-            {type}
+          <Link
+            className="grow"
+            key={type}
+            onClick={onQuizTypeClick}
+            href={`quiz/${type}`}
+          >
+            <p className="w-full h-full flex justify-center items-center rounded bg-[#2d2d2d]">
+              {(titles as Record<number, string>)[type]}
+            </p>
           </Link>
         ))}
       </div>
